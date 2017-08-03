cf4217d3c5fb61b34a077528213078bd47961b1a
adminuser <adminuser@localhost.localdomain>
2017-08-03 18:06:10 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 let { mongoose } = require('./db/mongoose.js');
+
 let { Todo } = require('./models/todo');
 let { User } = require('./models/user');
 
@@ -16,6 +18,33 @@ app.post('/todos', (req, res) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({ todos });
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  };
+
+  Todo.findById(id).then((todo) => {
+    if (todo) {
+      res.send({ todo });
+    } else {
+      return res.status(404).send();
+    }
+
+  }, (e) => {
+    res.status(400).send();
   });
 });
 
